2444f7b994decbeccca7b969bfc6d7c37b9a5bfa
Girmay-1 <girmaykibreab93@gmail.com>
2024-08-06 22:48:11 -0700

    basic parsing fuction implemented

---
+++ parser.lua
@@ -0,0 +1,34 @@
+local Parser = {}
+
+function Parser.parse(input)
+    local words = {}
+    for word in input:gmatch("%w+") do
+        table.insert(words, word:lower())
+    end
+
+    if #words == 0 then
+        return nil, "No command given"
+    end
+
+    local command = words[1]
+    local object = words[2]
+
+    -- Basic command recognition
+    if command == "go" or command == "move" then
+        if object then
+            return "move", object
+        else
+            return nil, "Go where?"
+        end
+    elseif command == "look" or command == "examine" then
+        if object then
+            return "examine", object
+        else
+            return "look"
+        end
+    elseif command == "take" or command == "get" then
+        if object then
+            return "take", object
+        else
+            return nil, "Take what?"
+        end
